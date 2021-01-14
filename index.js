9511f5cc61d484ddaf32c76a42b4a68cdbf234a6
Ihor Rudakov <i.rudakov2000@gmail.com>
2021-01-14 16:16:17 +0200

    Learn more about Promises

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
 const rootElement = document.getElementById('root');
+const loadingElement = document.getElementById('loading-overlay');
 
-rootElement.innerText = 'Loading...'
 fetch(API_URL)
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            return Promise.reject(new Error('Failed load data'));
+        }
+        return response.json();
+    })
     .then(file => {
         const fighters = JSON.parse(atob(file.content));
         const names = fighters.map(it => it.name).join('\n');
-
         rootElement.innerText = names;
+    })
+    .catch(error => {
+        console.warn(error);
+        root.innerText = 'Failed to load data';
+    })
+    .finally(() => {
+        loadingElement.remove(); 
     });
