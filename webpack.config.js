0e7a7a8fc8ed8aa8e0a03b1040e26f6c8e7f3cb1
Ihor Rudakov <i.rudakov2000@gmail.com>
2021-01-15 14:37:18 +0200

    Lection over

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), 
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: [
+                {
+                    loader: "babel-loader",
+                    options: {
+                        configFile: "./babel.config.js",
+                        cacheDirectory: true
+                    }
+                }
+            ]
+        }
+    ]
+  },
+  mode: 'development',
+  devServer: {
+    inline: true
+  },
+  devtool: "source-map"
+}
